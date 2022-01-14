12d5fd01dd7dc7d11b95b3daf96b38e719fde2bf
Glitch (tricky-frill-marlin) <none>
2022-01-14 12:21:43 +0000

    🛫🍣 Checkpoint ./commands/core/help.js:41066125/182

--- commands/core/help.js
+++ commands/core/help.js
@@ -14,7 +14,7 @@ module.exports = {
         embed.setThumbnail(client.user.displayAvatarURL())
         const commands = client.commands.filter(x => x.showHelp !== false);
 
-        embed.setDescription('To access this music bot: [Umut Bayraktar Music Bot](https://youtube.com/UmutBayraktarYT), need more help You can get help by joining our Discord Community.\nHow about creating your own music world?') ;
+        embed.setDescription('Seraphine in your area ! .\nHow about creating your own music world?') ;
         embed.addField(`Available - ${commands.size} Command Available`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
 
         embed.setTimestamp();
